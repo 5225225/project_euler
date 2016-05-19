c75bd4107a57ab3d8dc9c5417a906093d7516ed6
5225225 <5225225@users.noreply.github.com>
2016-05-19 21:13:43 +0100

    Add factors function to util.py

--- util.py
+++ util.py
@@ -1,3 +1,6 @@
+import math
+
+
 def fib(up_to=None):
     a = 0
     b = 1
@@ -10,3 +13,16 @@ def fib(up_to=None):
                 yield a
             else:
                 break
+
+
+def factors(x):
+    factors = []
+    while True:
+        for y in range(2, int(math.sqrt(x)) + 1):
+            if x % y == 0:
+                x = x // y
+                factors.append(y)
+                break
+        else:
+            factors.append(x)
+            return factors
